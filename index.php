1442db570b4947d3d65b348e5aef2c52488280a8
gappollo <156314879+gappollo@users.noreply.github.com>
2024-09-26 09:27:35 +0200

    Update index.php
    added RingCentral code

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 echo "Hello! This is a basic testing page for Azure App services. It is developed in PHP";
+
+<script>
+  (function() {
+    var rcs = document.createElement("script");
+    rcs.src = "https://apps.ringcentral.com/integration/ringcentral-embeddable/latest/adapter.js";
+    var rcs0 = document.getElementsByTagName("script")[0];
+    rcs0.parentNode.insertBefore(rcs, rcs0);
+  })();
+</script>
